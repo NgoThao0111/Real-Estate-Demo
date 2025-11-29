14fb35b049f3e82aababae839b15ceaf89c4175c
Hoang Van Binh <hoangvanbinh141205@gmail.com>
2025-11-29 13:11:28 +0700

    feat marker and popup, appear data in map

--- backend/server.js
+++ backend/server.js
@@ -47,8 +47,8 @@ const io = new Server(httpServer, {
 });
 
 // --- MIDDLEWARE CHO EXPRESS APP ---
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(cookieParser()); 
 
 // CORS setup
