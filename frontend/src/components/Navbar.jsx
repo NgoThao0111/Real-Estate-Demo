ac3d6c3aeaa0a40887ef61668a1f6ea61091e9be
NgoThao0111 <ngothao0111nt@gmail.com>
2025-12-17 22:52:32 +0700

    fix: move logo to public

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -51,7 +51,7 @@ const Navbar = () => {
           {/* Left: Logo */}
           <Link to="/">
             <HStack>
-              <Image src="/logo/logo.png" alt="Logo" h="40px" />
+              <Image src="/public/logo.png" alt="Logo" h="40px" />
               <Text
                 fontSize="24px"
                 fontWeight="bold"
